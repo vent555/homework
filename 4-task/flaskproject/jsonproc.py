0ccb505b0562f6eb9c021b7dbc572d3cefcb8b57
Vitaliy Sidorenko <vent555@yandex.ru>
2021-02-23 14:38:38 +0300

    task 4, change app to display value of dictionary var

--- 4-task/flaskproject/jsonproc.py
+++ 4-task/flaskproject/jsonproc.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
 
+people = [{'name': 'Alice', 'birth-year': 1986},
+          {'name': 'Bob', 'birth-year': 1985}]
+
 app = Flask(__name__)
 
 @app.route('/api/get-json')
 def hello():
-    return jsonify(hello='world') # Returns HTTP Response with {"hello": "world"}
+    return jsonify(people)
