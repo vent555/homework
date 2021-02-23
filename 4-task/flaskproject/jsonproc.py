bf4d7ef87e71dabfbcec61cb0095af7ee4e1cceb
Vitaliy Sidorenko <vent555@yandex.ru>
2021-02-23 20:01:23 +0300

    task 4, add functionality processing POST request
    *add to output emoji_word_emoji sequence based on value of count from POST request

--- 4-task/flaskproject/jsonproc.py
+++ 4-task/flaskproject/jsonproc.py
@@ -1,11 +1,17 @@
 from flask import Flask, jsonify, request
 from emoji import emojize
 
-
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST', 'DELETE', 'PUT'])                                                                                                    
-def add():                                                                                                                              
+def add():
+    a = 1
+    strout = emojize(":thumbs_up:")                                                                                                                              
     data = request.get_json()
-    strout = emojize(":thumbs_up:") + data["word"] + emojize(":thumbs_up:")
+    while a <= data["count"]:
+        strout = strout + data["word"] + emojize(":thumbs_up:")
+        a += 1
+
+    strout = strout + '\n'
+
     return strout
