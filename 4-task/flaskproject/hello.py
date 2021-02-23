f15e3862d2632fd37cb375911fbeafb2287778e0
Vitaliy Sidorenko <vent555@yandex.ru>
2021-02-23 13:25:26 +0300

    task 4, add template for app "hello, world"

--- 4-task/flaskproject/hello.py
+++ 4-task/flaskproject/hello.py
@@ -1,6 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
+from flask import render_template
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'index'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
