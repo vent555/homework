8f9489e3f46f52d773ae2a835b1fb4d72cb33947
Vitaliy Sidorenko <vent555@yandex.ru>
2021-02-25 16:26:52 +0300

    task 5: add ansible config files and project files for deploy
    +add project/* that will used for deploy service
    +add ansible inventory and configuration files: hosts and ansible.cfg

---
+++ 5-task/project/jsonproc.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+from emoji import emojize
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def method_get():
+     return '''
+Hello! Try to use POST request for more fun.
+Send me something like this: {"word":"everything", "count": 3}
+Don't forget to point content type "application/json".
+'''
+
+@app.route('/', methods=['POST'])
+def method_post():
+    a = 1
+    strout = emojize(":thumbs_up:")
+    data = request.get_json()
+    while a <= data["count"]:
+        strout = strout + data["word"] + emojize(":thumbs_up:")
+        a += 1
+
+    strout = strout + '\n'
+
+    return strout
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
