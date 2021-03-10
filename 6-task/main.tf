fe2cfdc52b781778690630c73d52471675027b7f
Vitaliy Sidorenko <vent555@yandex.ru>
2021-03-10 12:59:02 +0300

    task 6, add input and output variables usage

--- 6-task/main.tf
+++ 6-task/main.tf
@@ -1,20 +1,32 @@
+variable "server_port" {
+  description   = "Server port to listen http requests"
+  type          = number
+  default       = 8080
+}
+#output server public IP address 
+output "public_ip" {
+  #link variable with ec2 instance
+  value = aws_instance.test-1.public_ip
+  description = "Public ip of the web server"
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
 
 resource "aws_instance" "test-1" {
-  ami = "ami-0767046d1677be5a0"
-  instance_type = "t2.micro"
+  ami                      = "ami-0767046d1677be5a0"
+  instance_type            = "t2.micro"
   #to link ec2-instance with security group:
-  vpc_security_group_ids = [ aws_security_group.instance.id ]
+  vpc_security_group_ids   = [ aws_security_group.instance.id ]
   
   #create start-on-boot script
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  tags = {
+  tags      = {
     "Name" = "terraform-example"
   }
 }
@@ -24,8 +36,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }  
