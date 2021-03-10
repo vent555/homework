7e48f698dc07fee86bd867592ca929e699896a71
Vitaliy Sidorenko <vent555@yandex.ru>
2021-03-10 12:12:50 +0300

    task 6, add security group and web-app

--- 6-task/main.tf
+++ 6-task/main.tf
@@ -5,8 +5,28 @@ provider "aws" {
 resource "aws_instance" "test-1" {
   ami = "ami-0767046d1677be5a0"
   instance_type = "t2.micro"
+  #to link ec2-instance with security group:
+  vpc_security_group_ids = [ aws_security_group.instance.id ]
   
+  #create start-on-boot script
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
     "Name" = "terraform-example"
   }
+}
+
+#this resource exports attribute "id" to refer on them.
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
 }
